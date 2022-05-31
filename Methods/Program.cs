58cff2d8cd2e2f9c3b1aad93f891e0e8793ce37c
Mobin Skaria <mobin0@berkeley.edu>
2022-05-31 11:57:10 -0700

    methods

---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine(Sum(5, 6, 7));
+            Console.WriteLine(Multiply(5, 6, 7));
+        }
+
+        public static int Sum(params int[] nums) {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                result += nums[i];
+
+            }
+            return result;
+        }
+        public static int Multiply(params int[] nums)
+        {
+            int result = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result *= nums[i];
+            }
+            return result;
+        }
+    }
+}
